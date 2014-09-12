54b499e82abae28629ff5784783a622b7dbae899
Prasanta Pal <prasanta.pal@gmail.com>
2014-09-12 16:06:26 -0400

    Error checkers implemented

--- index.cpp
+++ index.cpp
@@ -1,2 +1,141 @@
-#include "index.h"
+	#include "index.h"
+	
+	 
+		void Index::IndexHelper(){//Helper functions for ctors
+			state_signal_.connect(boost::bind(&Index::setIsReadyToRun,this));
 
+			std::string current_flag = "";
+			
+			is_source_file_name_set_ = false;
+			current_flag = "is_source_file_name_set_";
+			run_flag_list_[current_flag] = is_source_file_name_set_;
+			explicit_list_of_check_flags_.push_back(current_flag);
+			
+			is_target_file_name_set_ = false;
+			current_flag = "is_target_file_name_set_";
+			run_flag_list_[current_flag] = is_target_file_name_set_;
+			explicit_list_of_check_flags_.push_back(current_flag);
+
+			num_threads_ = 1;//Default config
+			is_num_threads_set_ = true;
+			current_flag = "is_num_threads_set_";
+			run_flag_list_[current_flag] = is_num_threads_set_;
+			explicit_list_of_check_flags_.push_back(current_flag);
+			is_ready_to_run_ = false;
+
+		}
+
+		Index::Index(){//ctor
+			IndexHelper();
+		}
+
+		void Index::setSourceFileName(const std::string source_file_name){
+			std::string current_flag = "";
+			source_file_name_ = source_file_name;
+			is_source_file_name_set_ = true;
+			
+			current_flag = "is_source_file_name_set_";
+			run_flag_list_[current_flag] = is_source_file_name_set_;
+
+			state_signal_();
+		}
+		
+		std::string Index::getSourceFileName() const {
+			return source_file_name_;
+		}
+
+		bool Index::getIsSourceFileNameSet() const {
+
+			return is_source_file_name_set_;
+			}
+
+		void Index::setTargetFileName(const std::string target_file_name){
+				std::string current_flag = "";
+				target_file_name_ = target_file_name;
+				is_target_file_name_set_ = true;
+
+				current_flag = "is_target_file_name_set_";
+				run_flag_list_[current_flag] = is_source_file_name_set_;
+
+				state_signal_();
+			}
+		
+		std::string Index::getTargetFileName() const {
+			return target_file_name_;
+			}
+
+		bool Index::getIsTargetFileNameSet() const {
+			return is_target_file_name_set_;
+			}
+
+		void Index::setNumThreads(int n){
+			std::string current_flag = "";
+			num_threads_ = n;
+			if(n > 0 ){
+				is_num_threads_set_ = true;
+				current_flag = "is_num_threads_set_";
+				run_flag_list_[current_flag] = is_source_file_name_set_;
+			}else {
+				is_num_threads_set_ = false;
+			}
+
+			state_signal_();
+
+			}
+			
+		int Index::getNumThreads() const {
+			return num_threads_;
+			}
+			
+		bool Index::getIsNumThreadsSet() const {
+			return is_num_threads_set_;
+			}
+			
+		void Index::setIsReadyToRun(){//Check for all conditionals
+
+			 for(auto i : explicit_list_of_check_flags_ ){ 
+
+				if( run_flag_list_[i] != true){
+
+					is_ready_to_run_ = false;
+					return;
+				}
+
+			}
+
+			is_ready_to_run_ = true;
+			}
+			
+		bool Index::isReadyToRun() const {
+			return is_ready_to_run_;
+			}
+
+		void Index::IndexHelp(){
+
+			std::cout << "Welcome to file indexing" << std::endl;
+
+			std::cout << "ctors Index()" << std::endl;
+			
+			if(getIsSourceFileNameSet())
+				std::cout << "source file-->" << getSourceFileName() << std::endl;
+
+			if(getIsTargetFileNameSet())
+				std::cout << "source file-->" << getTargetFileName() << std::endl;
+
+			if(getIsNumThreadsSet())
+				std::cout << "number of threads-->" << getNumThreads() << std::endl;
+
+
+
+
+		}
+		
+		void Index::IndexError(const std::string s){
+			std::cout << s << std::endl;
+		}
+		bool Index::isNumber(const std::string s){
+
+			return !s.empty() && std::find_if(s.begin(), 
+	        s.end(), [](char c) { return !std::isdigit(c); }) == s.end();
+
+		}
--- index_test.cpp
+++ index_test.cpp
@@ -8,6 +8,9 @@ int main(int argc, char** argv) {
 
 
 	int help_index = 1;
+	std::string source_file_input;
+	std::string target_file_input;
+	int num_threads = 1;
 
 	if(argc == 1 || (argc == 2 && (strncmp(argv[help_index],"--help",6) == 0 || strncmp(argv[help_index],"-help",5) == 0 || strncmp(argv[help_index],"help",4) == 0))){
 	
@@ -16,6 +19,57 @@ int main(int argc, char** argv) {
 		
 	}
 	 
+	Index index;
+	
+	switch(argc){
+		case 2:
+			source_file_input = argv[1];
+			index.setSourceFileName(source_file_input);
+
+		break;
+		case 3:
+			source_file_input = argv[1];
+			index.setSourceFileName(source_file_input);
+			target_file_input = argv[2];
+			index.setTargetFileName(target_file_input);
+
+		break;
+
+		case 4:
+			source_file_input = argv[1];
+			index.setSourceFileName(source_file_input);
+			target_file_input = argv[2];
+			index.setTargetFileName(target_file_input);
+			//check for valid positive integer 
+			if(Index::isNumber(argv[3])){
+				
+				num_threads = boost::lexical_cast<int>(argv[3]);
+				if(num_threads > 0)
+					index.setNumThreads(num_threads);
+				else {
+					index.IndexError("Number of threads being " + boost::lexical_cast<std::string>(num_threads) + " is not acceptable. Program quitting.");
+					return -1;
+				}
+
+			}
+			else {
+				index.IndexError("Number of threads is not a valid number. Program quitting.");
+				return -1;
+			}
+		break;
+	}
+
+	//index.setIsReadyToRun();
+
+	if(index.isReadyToRun() ){ 
+		index.IndexHelp();
+		std::cout << "program is ready to run " << std::endl;
+
+	} else {
+		std::cout << "program is not ready to run " << std::endl;
+		return -1;
+	} 
+
 	return 0;
 	
 	std::ifstream is;
